9a5fab09461e6d28c6169358ba42d97e9816756b
zirconium.n <zirconium.n@gmail.com>
2018-09-27 13:52:12 +0800

    change dds to png

--- js/tangerine/load_field_tiles.js
+++ js/tangerine/load_field_tiles.js
@@ -4,13 +4,13 @@ if (THREE == null) {
 
 var TGR = TGR || {};
 
-TGR.fieldLoader = new THREE.DDSLoader();
+TGR.fieldLoader = new THREE.TextureLoader();
 TGR.fieldLoader.setPath("texture/field/")
 
 TGR.Texture = TGR.Texture || {};
 TGR.Material = TGR.Material || {};
 
-TGR.Texture.bonus0 = TGR.fieldLoader.load("mass_bonus0.dds");
+TGR.Texture.bonus0 = TGR.fieldLoader.load("mass_bonus0.png");
 TGR.Material.bonus0 = new THREE.MeshBasicMaterial({map : TGR.Texture.bonus0}); 
 
 delete TGR.fieldLoader;
